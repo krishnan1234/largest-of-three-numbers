2791fba46b7839285b44aee725058e7016579cc7
santhana krishnan <69023009+krishnan1234@users.noreply.github.com>
2021-04-26 00:11:43 -0700

    Create large.c

---
+++ large.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b){
+        printf("%d",a);
+    }
+    else if(b>c){
+        printf("%d",b);
+    }
+    else{
+        printf("%d",c);
+    }
+    return 0;
+}
